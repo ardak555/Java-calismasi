413da8f7dbb6c73a507ad5af0807da513971cc9b
Arda Koyuncu <79996371+ardak555@users.noreply.github.com>
2022-06-30 22:41:52 +0300

    Add files via upload

---
+++ sesliharf.java
@@ -0,0 +1,24 @@
+public class sesliharf {
+    public static void main(String[] args) throws Exception {
+        char harf='A';
+
+        switch(harf){
+            case 'A':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.println("Kalın sesli harf");
+                break;
+            case 'E':
+            case 'İ':
+            case 'Ö':
+            case 'Ü':
+                System.out.println("İnce sesli harf");
+                break;
+            default:
+                System.out.println("Sesiz harf.");
+                break;
+
+        }
+    }
+}
